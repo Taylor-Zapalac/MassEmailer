98efe16bf0657e72c45844884691cc08eee4ecc8
Taylor-Zapalac <Taylor.Zapalac@gmail.com>
2018-11-16 15:16:45 -0600

    Initial upload

---
+++ MassEmailer/Emailer.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.Net.Mail;
+
+namespace MassEmailer
+{
+    class Emailer
+    {
+
+        public void SendEmail(string emailSubject, string emailAddress, string emailBody, string relayClient, string fromEmail)
+        {
+            SmtpClient client = new SmtpClient(relayClient);
+            MailAddress from = new MailAddress(fromEmail);
+            MailAddress to = new MailAddress(emailAddress);
+            MailMessage body = new MailMessage(from, to);
+            body.Body = emailBody;
+            body.Subject = emailSubject;
+            client.Send(body);
+
+            body.Dispose();
+        }
+    }
+}
---
+++ MassEmailer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassEmailer
+{
+    class Program
+    {
+        // Replace what you need, then run.
+        // emailBody - The body of your email
+        // SMTPCLIENT - your relay address
+        static void Main(string[] args)
+        {
+            Emailer emailer = new Emailer();
+            String SMTPCLIENT = "insert your relay here";
+
+            List<String> emailList = new List<string>();
+            String emailBody = "";
+
+            foreach (string email in emailList)
+            {
+                emailer.SendEmail("Subject", email, emailBody, SMTPCLIENT, "insertYourEmailAddress@test.net");
+            }
+        }
+    }
+}
